344ae7c80347f09ddf08e15fc9cf66118df553c6
jfrey212 <eojyerf@gmail.com>
2022-03-08 11:06:49 -0600

    added project specs and created the sub method with two arguments

---
+++ sub.rb
@@ -0,0 +1,11 @@
+# Project description:
+# Implement a method #substrings that takes a word as the first argument and
+# then an array of valid substrings (your dictionary) as the second argument. It
+# should return a hash listing each substring (case insensitive) that was found in
+#  the original string and how many times it was found.
+
+# word is a string, dictionary is a array of substrings
+
+def sub(word, dictionary)
+
+end
